0bc7b9ab0f852c60d1c4b233084352e2d347ff46
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-17 00:42:25 -0300

    Se creó el componente Mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Mensaje from './components/Mensaje'
 
 function App() {
   return (
-   <div></div>
+   <>
+      <Mensaje></Mensaje>
+   </>
   )
 }
 
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mensaje = () => {
+    return (
+        <section>
+            <h1 >Hello Word!</h1>
+            
+        </section>
+    );
+};
+
+export default Mensaje;
